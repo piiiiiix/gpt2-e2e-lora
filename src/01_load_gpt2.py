da68da9d09bdda3dac38f0665af477d7fa449b80
piiiiiix <2399124693@qq.com>
2026-09-18 19:17:41 +0800

    env test

---
+++ src/01_load_gpt2.py
@@ -0,0 +1,43 @@
+import torch
+from transformers import AutoTokenizer , AutoModelForCausalLM
+
+model_name = "openai-community/gpt2"
+
+# 加载模型对应的tokenizer
+print("1. Loading tokenizer...")
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+# 加载模型
+print("2. Loading GPT-2...")
+model = AutoModelForCausalLM.from_pretrained(model_name)
+# 把模型搬到显卡
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model = model.to(device)
+
+print("3. Device:",device)
+
+prompt = "The meaning of life is"
+
+# 切分prompt，把切分好的token转成token ID，再包装成PyTorch tensor这个PT
+inputs = tokenizer(prompt, return_tensors = "pt")
+# 把输入搬进显卡
+inputs = inputs.to(device)
+
+# 输出token ID
+print("4. Input IDs:")
+print(inputs["input_ids"])
+
+print("5. Tokens:")
+# 每人看一眼token ID所对应的token
+print(tokenizer.convert_ids_to_tokens(inputs["input_ids"][0]))
+
+with torch.no_grad():  # 不进行反向传播
+    outputs = model.generate(  # 模型生成，词向量转换的过程被包含在内
+        **inputs,
+        max_new_tokens = 30,  # 最多30个token
+        do_sample = False,  # 贪心,不进行随机采样
+    )
+
+# 解码模型输出,跳过特殊token
+print("6. Generated text:")
+print(tokenizer.decode(outputs[0], skip_special_tokens = True))
--- src/test.py
+++ src/test.py
@@ -1,3 +1,8 @@
-import sys
+import torch, transformers, datasets, peft
 
-print(sys.executable)
+print('torch:', torch.__version__)
+print('transformers:', transformers.__version__)
+print('datasets:', datasets.__version__)
+print('peft:', peft.__version__)
+print('cuda:', torch.cuda.is_available())
+print('gpu:', torch.cuda.get_device_name(0))
